7ba15a8559aafe4559216b749ee9867cc3346961
mathislrn <mathis.lrn061198@gmail.com>
2021-04-16 12:29:06 +0200

    Placing airports done

--- src/script.js
+++ src/script.js
@@ -5,64 +5,41 @@ const { TrackballControls } = require('three/examples/jsm/controls/TrackballCont
 import ThreeGlobe from 'three-globe';
 import airportsData from '../Data/airports.json';
 
-const convertLatLngToPolarCoordinate = (lat, lng, sphereRadius) => {
-	let x = 6371 * Math.cos(lat) * Math.cos(lng)
-	let y = 6371 * Math.cos(lat) * Math.sin(lng)
-	let z = 6371 * Math.sin(lat)
-
-	console.log([x, y, z])
-  	return [x, y, z];
-}
-
 let airportsDataFormatted = [];
 let airportsLocations = [];
 airportsData.forEach((airport) => {
-  airportsDataFormatted[airport["IATA/FAA"]] = {lat: parseFloat(airport.Latitude), lng: parseFloat(airport.Longitude)};
+  	airportsDataFormatted[airport["IATA/FAA"]] = {
+		lat: parseFloat(airport.Latitude), 
+		lng: parseFloat(airport.Longitude)
+	};
 
-  if (airport["IATA/FAA"] = 'NTE') {
-		let convertedAirportData = convertLatLngToPolarCoordinate(parseFloat(airport.Latitude), parseFloat(airport.Longitude), Math.random() / 3);
-		let airportLocation = {
-			lat: convertedAirportData[0],
-			lng: convertedAirportData[1],
-			size: convertedAirportData[2],
-			color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
-		}
-		airportsLocations.push(airportLocation);
+	let airportLocation = {
+		lat: parseFloat(airport.Latitude),
+		lng: parseFloat(airport.Longitude),
+		size: 1,
+		color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
 	}
+	airportsLocations.push(airportLocation);
 });
-// console.log(airportsDataFormatted);
-
-/*const test = [...airportsDataFormatted.keys()].map((airport) => ({
-  lat: conv,
-  lng: (Math.random() - 0.5) * 360,
-  size: Math.random() / 3,
-  color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
-}));
-console.log(test);*/
-
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
-// Gen random data
-/*const N = 300;
-const airportsLocations = [...Array(N).keys()].map(() => ({
-  lat: (Math.random() - 0.5) * 180,
-  lng: (Math.random() - 0.5) * 360,
-  size: Math.random() / 3,
-  color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
-}));*/
-
 const Globe = new ThreeGlobe()
-  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
-  .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
-  .pointsData(airportsLocations)
-  .pointAltitude('size')
-  .pointColor('color');
+	.globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
+	.bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+	/*.pointsData(airportsLocations)
+	.pointAltitude('size')
+	.pointColor('color');*/
+	.labelsData(airportsLocations)
+	.labelText(d => `(${Math.round(d.lat * 1e2) / 1e2}, ${Math.round(d.lng * 1e2) / 1e2})`)
+	.labelSize('size')
+	.labelDotRadius(d => d.size / 5)
+	.labelColor('color');
 
 setTimeout(() => {
-  airportsLocations.forEach(d => d.size = Math.random());
-  Globe.pointsData(airportsLocations);
+  	airportsLocations.forEach(d => d.size = Math.random());
+  	Globe.pointsData(airportsLocations);
 }, 4000);
 
 /**
